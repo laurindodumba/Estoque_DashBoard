1b33419a892286cea39808ce64a236689ec65fd7
Laurindo Dumba <dumbalvd@gmail.com>
2025-01-28 14:57:10 -0300

    Criação dos Dashboard

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import yfinance as yf
 import plotly.express as px
+import numpy as np
 import streamlit as st
 from datetime import datetime
 from streamlit_extras.metric_cards import style_metric_cards
@@ -42,7 +43,42 @@ def build_main(tickers, prices):
     else:
         st.warning("Nenhum dado disponível. Selecione ao menos uma empresa.")
 
+    weights = np.ones(len(tickers))/len(tickers)
+    prices['portifolio'] = prices @ weights
+    norm_prices = 100 * prices / prices.iloc[0]
+    returns = prices.pct_change()[1:]
+    vols = returns.std()*np.sqrt(252)
+    rets = (norm_prices.iloc[-1] * 100) / 100
 
+    # Criaçã do primeiro gráfico
+    col1, col2 = st.columns(2, gap='large')
+    with col1:
+        st.subheader("Desempenho Relativo")
+        st.line_chart(norm_prices, height=600)
+
+
+    # Criação do segundo gráfico
+    with col2:
+        st.subheader("Risco Retorno")
+        fig = px.scatter(
+            x=vols,
+            y=rets,
+            color=rets/vols,
+            color_continuous_scale=px.colors.sequential.Bluered_r
+        )
+        fig.update_traces(
+            textfont_color='white',
+            marker=dict(size=45),
+            textfont_size=10,
+        )
+        fig.layout.yaxis.title = 'Retorno Total'
+        fig.layout.xaxis.title = 'Volatilidade (Anual)'
+        fig.layout.height = 600
+        fig.layout.yaxis.tickformat = ".0%"
+        fig.layout.xaxis.tickformat = ".0%"
+
+        fig.layout.coloraxis.colorbar.title = 'Sharpe'
+        st.plotly_chart(fig, use_container_width=True)
 # Criar um contexto para o SideBar
 with st.sidebar:
     tickers, prices = build_sidebar()
